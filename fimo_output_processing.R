e2e672276e36bc1f909154eb68b214a74cb0f94d
antortjim <ntoniohu@gmail.com>
2016-05-22 20:18:54 +0200

    Connecting to Blast.R

--- fimo_output_processing.R
+++ fimo_output_processing.R
@@ -5,6 +5,7 @@ library("ggplot2")
 library("dplyr")
 library(devtools)
 library("seqinr")
+library("seqLogo")
 source_gist(4676064)
 
  arguments <- as.integer(commandArgs(trailingOnly = T))
@@ -154,18 +155,80 @@ data <- data[with(data, order(n.mismatch, significant, start)), ]
 
 data$Distance.to.TSS <- data$start - upstream.distance
 
-Alicia <- data %>% dplyr::select(TSS, Strand, Annotation, TSS.class, Distance.to.TSS, fold.change.WT, significant, sequence, n.mismatch)
+data <- rbind(data.frame(filter(expression.df, Annotation == "all1873"), start = 101, stop = 111,
+           sequence = palindromo, n.mismatch = 0, Distance.to.TSS = 1), data)
 
-write.table(Alicia, file = "palindromos_hasta_2_mismatches.txt", row.names = F)
+color.sequences <- data$sequence %>%
+                   as.character() %>%
+                   strsplit(split = "") %>% 
+                   as.data.frame 
+color.sequences
+
+# Problema: resolver problema de longitud de current.col
+# cuando la columna analizada no contiene en ninguna 
+# secuncia a una de las letras (resulta ser de longitud
+# menor al numero de filas de la matrix y da error
+
+alignment.analyzer <- function(alignment.df)
+ {
+   number.of.columns <- ncol(alignment.df)
+   conservation <- matrix(0, ncol = number.of.columns, nrow = 4)
+   base.code <- 1:4
+   names(base.code) <- c("A","C","G","T")
+   for (column in 1:number.of.columns)
+   {
+     #print(column)
+     current.col <- alignment.df[,column] %>% as.character() %>% table() / nrow(alignment.df)
+     present.bases <- names(current.col)
+     
+     for (i in 1:length(present.bases))
+     {
+       current.base <- present.bases[i]
+       #print(current.base)
+       conservation[base.code[current.base], column] <- current.col[[current.base]]
+
+     }
+   }
+   return(conservation)
+}
+
+pdf(file = "~/MEGA/CuartoCurso/TFG/Documento/TFG/Figures/seqlogo.pdf", height = 5)
+pwm <- alignment.analyzer(color.sequences) %>% makePWM() %>% seqLogo(xaxis = F, yaxis = F)
+dev.off()
+system("inkscape -f ~/MEGA/CuartoCurso/TFG/Documento/TFG/Figures/seqlogo.pdf -A ~/MEGA/CuartoCurso/TFG/Documento/TFG/Figures/seqlogo.pdf")
+
+#write.table(file = "palindromos_color.txt", row.names = F)
+
+#Alicia <- data %>% dplyr::select(TSS, Strand, Annotation, TSS.class, Distance.to.TSS, fold.change.WT, significant, sequence, n.mismatch)
+
+#write.table(Alicia, file = "palindromos_hasta_2_mismatches.txt", row.names = F)
 
 data$symbol <- ifelse(data$Distance.to.TSS > 0, "+", "-")
+
+
 data$Distance.to.TSS <- abs(data$Distance.to.TSS)
+Distance.to.TSS <- paste(data$symbol, data$Distance.to.TSS)
 
-data <- rbind(data.frame(filter(expression.df, Annotation == "all1873"), start = 101, stop = 111,
-           sequence = palindromo, n.mismatch = 0, Distance.to.TSS = 1, symbol = "+"), data)
+tabular.data <- data %>% select(TSS.class:stop, n.mismatch) %>% cbind(Distance.to.TSS, color.sequences)
 
 
-write.table(dplyr::select(data, TSS, Strand, Annotation, TSS.class, symbol, Distance.to.TSS, fold.change.WT, sequence), file = "fimo_data.txt", row.names = F)
+#write.table(dplyr::select(data, TSS, Strand, Annotation, TSS.class, symbol, Distance.to.TSS, fold.change.WT, sequence), file = "fimo_data.txt", row.names = F)
+tabular.data <- select_(tabular.data, "TSS", "Strand", "Annotation", "TSS.class", "Distance.to.TSS", "fold.change.WT", "X1:X10", "n.mismatch")
+
+commandArgs <- function() list(tabular.data, 20)
+source("palindromo/tableR.R")
+
+
+blast <- readline('Would you like to blast interesting genes? Yes/No:\n')
+if(blast == "Yes")
+{
+  query.genes <- readline("Enter interesting genes: ") %>% strsplit(split = " ") %>% unlist()
+  commandArgs <- function() list(query.genes)
+  source("palindromo/blast.R")
+}
+
+
+
 
 sequences <- data$sequence %>% as.character() %>% as.list()
 seqs.id <- paste(data$TSS, data$Annotation, data$TSS.class, sep ="_")
@@ -185,7 +248,7 @@ ggsave("plot1_1.pdf")
 
 ggplot(data = data, aes(x = log10(WT0), y = log10(WT8), col = significant, shape = n.mismatch)) + 
   geom_point() +
-  scale_shape_manual(values = c(17, 19))
+  scale_shape_manual(values = c(17, 19,1))
 ggsave("plot1_2.pdf")
 ##Perfil de la distribucion del comienzo de los palindromos en el entorno de los TSS de interes
 ##Cuanto más alta sea la señal, es que la frecuencia con la que el TSS se posiciona ahí es mayor
@@ -198,12 +261,12 @@ ggsave("plot4.pdf")
 
 ggplot(data = data, aes(x = start - upstream.distance, col = significant)) + geom_density(size = 1.5) +
   xlab("Intervalo (-100, +50) respecto a los TSS") + ylab("Densidad") + theme_bw() +
-  scale_color_manual(name = "Actividad transcripcional\nen el estrés por nitrógeno",
+  scale_color_manual(name = "Actividad transcripcional\nen el estrés por carencia\nde nitrógeno",
                      labels = c("Disminuye","Aumenta","No significativo"),
                      breaks = c("decrease","increase","no"),
                      values = c("red","green","blue"))
 
-ggsave("../../Documento/TFG/Figures/plot5.pdf")
+ggsave("../Documento/TFG/Figures/plot5.pdf",  width = 10, height = 5, dpi = 120)
 
 ggplot(data = data, aes(x = significant, fill = significant)) + geom_bar()
 ggsave("plot6.pdf")
