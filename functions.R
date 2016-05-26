7b635f1d807bd1c35f1f4e53b7b5ab641d2cc48b
antortjim <ntoniohu@gmail.com>
2016-05-26 15:08:21 +0200

    A script containing code that implements functions required to process FIMO output in R

--- functions.R
+++ functions.R
@@ -1,3 +1,38 @@
+library("purrr") #convierte factores en characters
+library("Biostrings")
+library("ggplot2")
+library("dplyr")
+library(devtools)
+library("seqinr")
+library("seqLogo")
+source_gist(4676064)
+
+#Function that produces a matrix that seqlogo understands
+alignment.analyzer <- function(alignment.df)
+{
+  number.of.columns <- ncol(alignment.df)
+  conservation <- matrix(0, ncol = number.of.columns, nrow = 4)
+  base.code <- 1:4
+  names(base.code) <- c("A","C","G","T")
+  for (column in 1:number.of.columns)
+  {
+    #print(column)
+    current.col <- alignment.df[,column] %>% as.character() %>% table() / nrow(alignment.df)
+    present.bases <- names(current.col)
+    
+    for (i in 1:length(present.bases))
+    {
+      current.base <- present.bases[i]
+      #print(current.base)
+      conservation[base.code[current.base], column] <- current.col[[current.base]]
+      
+    }
+  }
+  return(conservation)
+}
+
+
+
 select.by.mismatches <- function(df, mypattern, mysubject, mismatch, colnames)
 {
   vmatchPattern.result <- vmatchPattern(pattern  = mypattern,
@@ -13,20 +48,27 @@ select.by.mismatches <- function(df, mypattern, mysubject, mismatch, colnames)
 
 break.by.underscore.fields <- function(big.set, underscore.fields.column, seqId.fields)
 {
-  small.set <- big.set[[underscore.fields.column]] %>% as.character() %>% strsplit(split = "_") %>% as.data.frame()
+  small.set <- big.set[[underscore.fields.column]] %>% as.character() %>% strsplit(split = "_") %>% as.data.frame.list()
   colnames(small.set) <- seqId.fields
   return(small.set)
 }
 
-
 count.reps <- function(df, feature.containing.repeats)
 {
   repeated.positions <- df[[feature.containing.repeats]] %>% as.list %>% duplicated
-  mydf <- data.frame((df[[feature.containing.repeats]][repeated.positions] %>% table) +1)
-  colnames(mydf) <- c(feature.containing.repeats, "total.count")
-  mydf <- full_join(df, mydf, by = feature.containing.repeats)
-  mydf[["total.count"]][is.na(mydf$total.count)] <- 1
-  return(mydf)
+  if(repeated.positions %>% sum != 0)
+  {
+   mydf <- data.frame((df[[feature.containing.repeats]][repeated.positions] %>% table) +1)
+   colnames(mydf) <- c(feature.containing.repeats, "total.count")
+   mydf <- full_join(df, mydf, by = feature.containing.repeats)
+   mydf[["total.count"]][is.na(mydf$total.count)] <- 1
+  }
+  else
+  {
+   df[["total.count"]] <- 1
+   mydf <- df
+  }
+    return(mydf)
 }
 
 
@@ -34,15 +76,24 @@ count.reps <- function(df, feature.containing.repeats)
 # {
 # }
 
-#Define the palindromic sequence
-palindromo <- "TACCCGGGTA"   # palindromo presente en el promotor de all1873
-# muy conservado en todas las cianos
-DNAS.palindromo <-  DNAString(palindromo)  # pasamos la secuencia del palindromo
-# a DNAString (Biostrings package)
-DNA.pattern <- DNAS.palindromo
 
-#Parse tss metadata structured by _ (Source_TSS_Annotation_TSS.class)
-seqId.fields <- c("Source","TSS","Annotation","TSS.class")
+
+complementary.reversal <- function(data, sequence.var = "matched.sequence", strand.var = "Strand")
+{
+  sequences <- data[[sequence.var]]
+  strand <- data[[strand.var]]
+  names(sequences) <- strand
+  for(i in 1:length(sequences))
+  {
+    if (names(sequences[i]) == "rev")
+    {
+      sequences[i] <- sequences[i] %>% s2c %>% rev %>% comp %>% toupper %>% c2s
+    }
+  }
+  data[[sequence.var]] <- sequences
+  return(data)
+}
+
 
 
 fimo.subject.sequences <- function(fimo)
@@ -58,13 +109,26 @@ fimo.subject.sequences <- function(fimo)
 }
 
 
+make.numeric.those.numeric <- function(df)
+{
+  right.order <- colnames(df)
+  df[] <- lapply(df, as.character)
+  digits.positions <- grepl("^[[:digit:]]+$", df[1,])
+  digits.df <- df[, digits.positions]
+  digits.columns <- colnames(digits.df)
+  digits.df[] <- lapply(digits.df, as.numeric)
+  df <- df[, !digits.positions]
+  df <- cbind(df, digits.df)
+  df <- df[,right.order]
+  return(df)
+}
 
 ##IMPORTANTE: DA ERROR SI FIMO CONTIENE INSTANCIAS DE TSS EN PLASMIDOS Y NO EN EL CROMOSOMA PRINCIPAL
 ##El error consiste en que quiere añadr columnas al data.frame relevant.tss.expression con mas
 ## filas que las que tiene la propia df
 
 ## This function organizes the fimo output by number of mismatches, and adds a significant expression tag based on threshold value
-fimo.output.integrator <- function(fimo.file, seqId.fields, DNA.pattern, DNA.subject, mismatch, expression, threshold = 3)
+fimo.output.integrator <- function(fimo.file, seqId.fields, DNA.pattern, mismatch, expression, threshold = 3)
 {
   #Lee la tabla y guardala en la variable fimo
   fimo <- read.table(fimo.file, header = T, sep = "\t", quote = "")
@@ -75,9 +139,11 @@ fimo.output.integrator <- function(fimo.file, seqId.fields, DNA.pattern, DNA.sub
   ## Genera una data.frame en la que cada fila representa un TSS y hay 4 columnas:
   ## source, coordenada, gen y tipo de tss, y ponles el nombre adecuado
   fimo.tss <- break.by.underscore.fields(fimo, "sequence.name", seqId.fields)
-  ## Añade esta data.frame a fimo (la principal) 
+
+  # Añade esta data.frame a fimo (la principal) 
   fimo <- cbind(fimo.tss, fimo %>% dplyr::select(-sequence.name))
-  fimo$TSS <- fimo$TSS %>% as.numeric
+  rm(fimo.tss)
+  fimo <- make.numeric.those.numeric(fimo)
   fimo <- fimo %>% filter(Source == "chr")
   ## Genera una data.frame analoga con los TSS que tienen un palindromo con el numero de mismatches predefinido
   ## en su entorno
@@ -95,112 +161,39 @@ fimo.output.integrator <- function(fimo.file, seqId.fields, DNA.pattern, DNA.sub
   fimo.subset <- fimo.subset %>% dplyr::select(-pasted.seq.ann)
   
   
-  ## Extrae datos de los TSS
-  ##Coordenadas. El resto esta guardado en fimo.subset porque son datos que no van dentro del nombre del TSS
-  coordinates <- fimo.subset$TSS
-  ## Nombre del gen
-  geneNames <- fimo.subset$Annotation
   
-  ## Start del palindromo
-  startPositions <- fimo.subset$start
-  ## Stop del palindromo
-  stopPositions  <- fimo.subset$stop
-
+  
   #haz un subset de expression.df que se quede con tss que no estan en los plasmidos (mejorar codigo para soportar plasmidos)
   #y haz un subset que solo muestre informacion sobre los tss que hemos seleccionado de fimo
   #con eso, filtramos la informacion transcriptomica de los tss que ha detectado fimo
   expression.df <- expression.df %>% filter(Source == "chr")
   #expression.df <- subset(expression.df, expression.df$TSS %in% fimo.subset$TSS)
+
+
+  df <- dplyr::inner_join(expression.df, fimo.subset, by = c("TSS", "Annotation", "TSS.class","Source"))
+  ##Correct the sequences found in the - strand to show their complementary (defaults to the + strand)
+  df <- complementary.reversal(df, sequence.var = "matched.sequence", strand.var = "Strand")
+  ##Correct the relative position of these sequences with respect to the TSS
+  df[df[["Strand"]] == "rev", "start"] <- df[df[["Strand"]] == "rev", "start"] + 10
+  df[df[["Strand"]] == "rev", "stop"]  <- df[df[["Strand"]] == "rev", "stop"]  + 10
   
-  df <- dplyr::full_join(expression.df, fimo.subset, by = c("TSS", "Annotation"))
-  expression.df[with(expression.df, order(TSS)), ]
-  fimo.subset[with(fimo.subset, order(TSS)), ]
-  
-  fimo.subset$TSS %>% class
-  expression.df$TSS %>% class
-  
-  vector1 <- relevant.tss.expression$TSS %>% as.character() %>% as.numeric()
-  vector2 <- hits.df.tss.subset$TSS %>% as.character() %>% as.numeric()
-  mynumbers <- match(vector1, vector2)
-  
-  
-  relevant.tss.expression$start <- relevant.tss.start[mynumbers]
-  relevant.tss.expression$stop  <- relevant.tss.stop[mynumbers]
-  relevant.tss.expression$sequence <- hits.df[relevant.tss.index,]$matched.sequence[mynumbers]
-  relevant.tss.expression$TSS.class <- hits.df.tss [relevant.tss.index,]$TSS.class[mynumbers]
-  relevant.tss.expression$Annotation <- relevant.tss.gene[mynumbers]
-  relevant.tss.expression$n.mismatch <- mismatch
-  
-  return(relevant.tss.expression)
+  return(df)
 }
 
 
-## Initialize the data.frame that will store extracted data
-data <- data.frame()
 
-## For every possible mismatch, extract data and add it to that of all other mismatches
-## fimo.output.integrator works row-based. fimo.output.integrator must be executed once per row. Each iteration adds a new row corresponding
-## to a differnet hit in fimo.txt
-for(mismatch in min.mismatch:max.mismatch)
+addDistanceAsCharacter <- function(df, var = "start", distance = upstream.distance.prom, length = 9, new.var = "Distance.to.TSS", formatted.var = "pretty.distance")
 {
-  print(mismatch)
-  current.mismatch <- fimo.output.integrator(DNAS.palindromo, DNAS.match.sequence, mismatch, hits.df = fimo, expression = expression.df[,-(3:4)], threshold)
-  data <- rbind(data, current.mismatch)
+  ## Create a new variale that stores the distance to the TSS. Negative numbers mean that the sequence is upstream of the TSS,
+  ## whereas a positive number means downstream of the TSS
+  df[[new.var]] <- df[[var]] - distance
+  
+  #Create a new vector called symbol where + means that the sequence is beyond the TSS and - that the palindrom is before the TSS
+  symbol <- ifelse(df[[new.var]] > 0, "+", "-")
+  
+  #Get the absolute value
+  new.var.abs <- abs(df[[new.var]])
+  #Paste the symbol and the absolute values into a character vector (thus 3 is displayed as +3, making it prettier in the table)
+  df[[formatted.var]] <- paste(symbol, new.var.abs)
+  return(df)
 }
-
-##Correct the sequences found in the - strand to show their complementary (defaults to the + strand)
-sequences <- data$sequence %>% as.character()
-for (row in 1:nrow(data))
-{
-  current.strand <- data$Strand[row]
-  if(current.strand == "rev")
-  {
-    current.sequence <- sequences[row] %>% as.character() %>% strsplit(split = "") %>% unlist()
-    current.sequence <- current.sequence %>% seqinr::comp() %>% rev() %>% toupper()
-    current.sequence <- paste(current.sequence, collapse = "") 
-    sequences[row] <- current.sequence
-  }
-}
-
-## Add them to data passing each position as a different column. Sequences must all be of equal length
-sequences <- lapply(sequences %>% as.list, function(x) unlist(strsplit(x, split = ""))) %>% as.data.frame
-data <- select(data, -sequence) %>% cbind(sequences)
-
-
-## Make the number of mismatches a factor (n.mismatch) so that ggplot can handle it
-data$n.mismatch <- factor(data$n.mismatch, levels = min.mismatch:max.mismatch)
-
-## Create a new variale that stores the distance to the TSS. Negative numbers mean that the sequence is upstream of the TSS,
-## whereas a positive number means downstream of the TSS
-data$Distance.to.TSS <- data$start - upstream.distance.prom
-## remove rownames because they are non sense
-rownames(data) <- NULL
-
-
-
-
-
-## REMOVING DUPLICATED PALINDROMES
-# This block of code removes sequences that are duplicated because there is more than one palindrom close. Only the first occurence in data is kept,
-##Get the coordinates of the palindrom (that of the TSS + the distance to the TSS)
-palindromes.coordinates <- data$TSS + data$Distance.to.TSS
-#Determine which of them are duplicated
-pal.coord <- !(palindromes.coordinates %>% duplicated.Vector())
-
-
-#Create a matrix where the first column is the vector palindromes coordinates, and the second column is the same +9 (encoding the end of the palindromes)
-#Call it the same (machaca el vector)
-palindromes.coordinates <- matrix(c(palindromes.coordinates, palindromes.coordinates + 9), ncol = 2)
-#Create a new vector called symbol where + means that the sequence is beyond the TSS and - that the palindrom is before the TSS
-symbol <- ifelse(data$Distance.to.TSS > 0, "+", "-")
-#Get the absolute value
-Distance.to.TSS.abs <- abs(data$Distance.to.TSS)
-#Paste the symbol and the absolute values into a character vector (thus 3 is displayed as +3, making it prettier in the table)
-data$Distance.to.TSS.pretty <- paste(symbol, Distance.to.TSS.abs)
-
-
-#Remove duplicates and order data. data.no.duplicates has no duplicates and is the one what will be used 
-data.no.duplicates <- data[pal.coord,]
-data <- data[with(data, order(n.mismatch, start)), ]
-data.no.duplicates <- data.no.duplicates[with(data.no.duplicates, order(n.mismatch, start)), ]
-
